a163706c9c3763cad441f63c8f5a26ff69516c5b
Julien Cornuwel <julien@cornuwel.net>
2017-11-05 17:56:49 +0100

    Wrap every nvidia-smi value in a pretty Struct

--- lib/nvidia-smi.rb
+++ lib/nvidia-smi.rb
@@ -1,60 +1,53 @@
-
 module NvidiaSmi
 
-  # :stopdoc:
-  LIBPATH = ::File.expand_path('..', __FILE__) + ::File::SEPARATOR
-  PATH = ::File.dirname(LIBPATH) + ::File::SEPARATOR
-  VERSION = ::File.read(PATH + 'version.txt').strip
-  # :startdoc:
+  @@nvidia_keys = [
+    'index',
+    'pci.bus',
+    'name',
+    'uuid',
+    'temperature.gpu',
+    'fan.speed',
+    'memory.used',
+    'memory.total',
+    'memory.free',
+    'utilization.memory',
+    'utilization.gpu',
+    'power.draw',
+    'power.limit',
+    'pstate',
+    'clocks.gr',
+    'clocks.sm',
+    'clocks.mem',
+    'clocks.video',
+    'vbios_version'
+  ]
 
-  # Returns the library path for the module. If any arguments are given,
-  # they will be joined to the end of the libray path using
-  # <tt>File.join</tt>.
-  #
-  def self.libpath( *args )
-    rv =  args.empty? ? LIBPATH : ::File.join(LIBPATH, args.flatten)
-    if block_given?
-      begin
-        $LOAD_PATH.unshift LIBPATH
-        rv = yield
-      ensure
-        $LOAD_PATH.shift
+  return_keys = @@nvidia_keys.map(&:clone).each do |s|
+    s.gsub!('.', '_')
+  end
+
+  Struct.new('Gpu', *return_keys)
+
+
+  def self.gpu_count
+    `nvidia-smi --query-gpu=count --format=csv,noheader -i 0`.to_i
+  end
+
+  def self.driver_version
+    `nvidia-smi --query-gpu=driver_version --format=csv,noheader -i 0`.strip
+  end
+
+  def self.gpus
+    gpus = Array.new
+    data = `nvidia-smi --query-gpu=#{@@nvidia_keys.join(',')} --format=csv,noheader`.each_line do |line|
+      values = line.strip.split(',').each do |v|
+        v.strip!
       end
+
+      gpus << Struct::Gpu.new(*values)
     end
-    return rv
+    return gpus
   end
 
-  # Returns the lpath for the module. If any arguments are given,
-  # they will be joined to the end of the path using
-  # <tt>File.join</tt>.
-  #
-  def self.path( *args )
-    rv = args.empty? ? PATH : ::File.join(PATH, args.flatten)
-    if block_given?
-      begin
-        $LOAD_PATH.unshift PATH
-        rv = yield
-      ensure
-        $LOAD_PATH.shift
-      end
-    end
-    return rv
-  end
-
-  # Utility method used to require all files ending in .rb that lie in the
-  # directory below this file that has the same name as the filename passed
-  # in. Optionally, a specific _directory_ name can be passed in such that
-  # the _filename_ does not have to be equivalent to the directory.
-  #
-  def self.require_all_libs_relative_to( fname, dir = nil )
-    dir ||= ::File.basename(fname, '.*')
-    search_me = ::File.expand_path(
-        ::File.join(::File.dirname(fname), dir, '**', '*.rb'))
-
-    Dir.glob(search_me).sort.each {|rb| require rb}
-  end
-
-end  # module NvidiaSmi
-
-NvidiaSmi.require_all_libs_relative_to(__FILE__)
+end
 
